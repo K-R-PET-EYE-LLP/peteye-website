04456dfe07f5c97f554e04ed5d979ad5e4d012c9
jithunmadhav <jithunmadhav1@gmail.com>
2024-03-23 22:01:49 +0530

    FAQ completed

--- src/Pages/SupportPage/SupportPage.jsx
+++ src/Pages/SupportPage/SupportPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../../Components/Header/Header'
 import { IoSearchOutline } from "react-icons/io5";
 import { TbTruckDelivery } from "react-icons/tb";
@@ -9,6 +9,8 @@ import { FiCheckSquare } from "react-icons/fi";
 import { FaRegMoneyBillAlt } from 'react-icons/fa';
 import Footer from '../../Components/Footer/Footer'
 import AOS from 'aos';
+import faqJson from '../../assets/json/faq.json'
+import { IoMdArrowDropdown } from "react-icons/io";
 
 const mainImg="https://res.cloudinary.com/djweedhpy/image/upload/v1709701963/Desktop/b477f547-f202-44e5-9b18-c480bb06b05c_1_gahf0z.png"
 
@@ -16,6 +18,32 @@ export default function SupportPage() {
     useEffect(() => {
         AOS.init({duration:500, delay:100});
       }, [])
+
+      const PetData=faqJson;
+      const [searchQuery, setSearchQuery] = useState('');
+      const [searchResults, setSearchResults] = useState([]);
+    
+      const handleSearch = (e) => {
+        const query = e.target.value;
+        console.log(searchQuery);
+        setSearchQuery(query);
+
+        const regex = new RegExp(query, 'i'); 
+        const results = PetData.filter(item =>
+          regex.test(item.Question) || regex.test(item.Answer)
+        );
+        setSearchResults(results);
+      };
+
+      const [openAnswers, setOpenAnswers] = useState([]);
+
+      const toggleAnswer = (index) => {
+        const newOpenAnswers = [...openAnswers];
+        newOpenAnswers[index] = !newOpenAnswers[index];
+        setOpenAnswers(newOpenAnswers);
+      };
+console.log("SEARCH_RES : ",searchResults);
+
   return (
     <div className="main">
         <Header></Header>
@@ -29,12 +57,43 @@ export default function SupportPage() {
             </div>
             <div className="support-main-sec-search-box" data-aos="fade-up">
                 <div className="search-box-container">
-                    <input type="text" placeholder='Have a Question' />
+                    <input type="text" placeholder='Have a Question'
+                    value={searchQuery}
+                    onChange={handleSearch}
+                     />
                     <button>
                     <IoSearchOutline className='icon'/>
                     </button>
                 </div>
             </div>
+            {searchResults.length==0 || searchQuery.trim()=='' ?
+            <div></div>
+
+            : 
+                <div className='search-result-main-div'>
+                
+        {searchResults.map((item, index) => (
+       <>
+       <div key={index} className='res-question-div'>
+        <div style={{display:'flex',justifyContent:'space-between'}}>
+       <div>{item.Question}</div>
+       <div>
+         <IoMdArrowDropdown
+           onClick={() => toggleAnswer(index)}
+           style={{ transform: openAnswers[index] ? 'rotate(180deg)' : 'rotate(0deg)',cursor:'pointer' }}
+           />
+       </div>
+        </div>
+       <div style={{ display: openAnswers[index] ? 'block' : 'none' }}>{item.Answer}</div>
+     </div>
+           </>
+        ))}
+    
+         </div>
+
+             }
+
+
             <div className="support-main-sec-img">
                 <img src={mainImg} alt=""  data-aos="fade-up"/>
             </div>
